50a34db9f8cd67329cae5c9dff1f99205fc1d2fa
rbnset <rbn.setiyawan@gmail.com>
2026-02-28 14:47:27 +0700

    layout view

--- app/Filament/Resources/Authors/Tables/AuthorsTable.php
+++ app/Filament/Resources/Authors/Tables/AuthorsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Authors\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\ImageColumn;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -75,6 +76,10 @@ class AuthorsTable
                 EditAction::make()
                     ->label('Edit')
                     ->icon('heroicon-o-pencil-square'),
+
+                ViewAction::make()
+                    ->label('Lihat')
+                    ->icon('heroicon-o-eye'),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
