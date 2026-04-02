885f5eb03199c29dac02cc4d10d79d3707c71834
rbnset <rbn.setiyawan@gmail.com>
2026-04-02 12:33:17 +0700

    push

--- app/Filament/Resources/Reviews/Pages/CreateReview.php
+++ app/Filament/Resources/Reviews/Pages/CreateReview.php
@@ -64,4 +64,9 @@ class CreateReview extends CreateRecord
             new ReviewDecisionForAuthor($review)
         );
     }
+
+    protected function getCreateAnotherFormAction(): \Filament\Actions\Action
+    {
+        return parent::getCreateAnotherFormAction()->hidden();
+    }
 }
